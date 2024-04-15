986911b2c5c03a772949dea8ab7e51dbb3c8db37
schoolpurposee <149581810+schoolpurposee@users.noreply.github.com>
2024-04-15 22:46:40 +0800

    Add files via upload

---
+++ Meditation App/README.md
@@ -0,0 +1,2 @@
+# Wellness-APP
+A responsive website which helps you meditate and take care of your mental health.
---
+++ Personality Quiz/questions.js
@@ -0,0 +1,69 @@
+const questions = [
+  {
+    "question": "Age range?",
+    "answer1": "under 18",
+    "answer1Total": "1",
+    "answer2": "18 - 30",
+    "answer2Total": "2",
+    "answer3": "over 30",
+    "answer3Total": "3"
+  },
+  {
+    "question": "How you often deal with your problems.",
+    "answer1": "Having someone to talk with.",
+    "answer1Total": "1",
+    "answer2": "Deal and fix the problem with someone",
+    "answer2Total": "2",
+    "answer3": "Ghosting everyone and fixing the problem all by yourself",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Select in which order you would value these \"Money, Love & Career",
+    "answer1": "Love, Career, Money",
+    "answer1Total": "1",
+    "answer2": "Money, Career, Love",
+    "answer2Total": "3",
+    "answer3": "Career, Love, Money",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Best Sentence to describe you?",
+    "answer1": "You feel humble and don't see negativity in everyone.",
+    "answer1Total": "3",
+    "answer2": "You consider yourself more superior than being insecure.",
+    "answer2Total": "2",
+    "answer3":
+      "You easily get angry",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which best describes your relationship with food",
+    "answer1": "You tend to buy instant foods.",
+    "answer1Total": "1",
+    "answer2": "You tend to eat junk foods secretly.",
+    "answer2Total": "2",
+    "answer3": "You prepare food and don't even look at the recipe.",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "You make plans with a friend and they cancel on you, what do you do?",
+    "answer1":
+      "Say \"whatever\" and continue the plan without them.",
+    "answer1Total": "3",
+    "answer2": "Feel hurt because you were looking forward to tonight.",
+    "answer2Total": "2",
+    "answer3": "No problem, you kinda wanted to stay home anyway.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which of the following colours do you like most?",
+    "answer1": "Black or white",
+    "answer1Total": "3",
+    "answer2": "Yellow or light blue",
+    "answer2Total": "2",
+    "answer3": "Red or orange",
+    "answer3Total": "1"
+  }
+]
